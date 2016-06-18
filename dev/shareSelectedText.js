2b34033378d877ee9e5e3d582f0061abc5756cb8
Vincent Loy <vincent.loy1@gmail.com>
2016-06-18 02:26:10 +0200

    We have a nice start here. Improve code orthogonality, logic, and add a lot of options

--- dev/shareSelectedText.js
+++ dev/shareSelectedText.js
@@ -4,6 +4,7 @@
 (function (exports) {
     'use strict';
 
+    // constants
     const TOOLTIP_HEIGHT = 50;
     const FACTOR = 1.33;
     const TWITTER_LIMIT_LENGTH = 140;
@@ -14,8 +15,19 @@
     const REAL_TWITTER_LIMIT = TWITTER_LIMIT_LENGTH - TWITTER_URL_LENGTH_COUNT -
         TWITTER_COMMAS - TWITTER_DOTS;
 
+    const SOCIAL = {
+        twitter: 'twitter',
+        buffer: 'buffer',
+        digg: 'digg',
+        linkedin: 'linkedin',
+        stumbleupon: 'stumbleupon'
+    };
+
+    // globals
     let tooltip;
     let parameters;
+    let pageUrl = window.location.href;
+    let selected = {};
 
     let extend = function (out) {
         out = out || {};
@@ -32,25 +44,16 @@
         return out;
     };
 
-    let sanitizeTweet = function (text) {
+    let sanitizeForTweet = function (text) {
+        if (!text) {
+            return '';
+        }
+
         if (text.length > REAL_TWITTER_LIMIT) {
             return text.substring(0, REAL_TWITTER_LIMIT) + '...';
         }
+
         return text.substring(0, REAL_TWITTER_LIMIT + TWITTER_DOTS);
-    };
-
-    let generateTwitterUrl = function (url, text) {
-        if (parameters.sanitize) {
-            text = sanitizeTweet(text);
-        }
-        return `https://twitter.com/intent/tweet?url=${url}&text="${text}"`;
-    };
-
-    let generateBufferUrl = function (url, text) {
-        if (parameters.sanitize) {
-            text = sanitizeTweet(text);
-        }
-        return `https://buffer.com/add?text=${text}&url=${url}`;
     };
 
     let hideTooltip = function () {
@@ -61,55 +64,93 @@
         tooltip.classList.add('active');
     };
 
-    let updateTooltip = function (rect, selected) {
+    let generateSocialUrl = function (socialType, text) {
+        if (parameters.sanitize) {
+            text = sanitizeForTweet(text);
+        }
+
+        let urls = {
+            twitter: `https://twitter.com/intent/tweet?url=${pageUrl}&text="${text}"`,
+            buffer: `https://buffer.com/add?text="${text}"&url=${pageUrl}`,
+            digg: `http://digg.com/submit?url=${pageUrl}&title=${text}`,
+            linkedin: `https://www.linkedin.com/shareArticle?url=${pageUrl}&title=${text}`,
+            stumbleupon: `http://www.stumbleupon.com/submit?url=${pageUrl}&title=${text}`
+        };
+
+        return urls[socialType];
+    };
+
+    let updateTooltip = function (rect) {
         let actualPosition = document.documentElement.scrollTop || document.body.scrollTop;
         let body = document.querySelector('body');
 
         tooltip.style.top = actualPosition + rect.top - (TOOLTIP_HEIGHT * FACTOR) + 'px';
         tooltip.style.left = (rect.left + (rect.width / 2) - (body.getBoundingClientRect().width / 2)) + 'px';
 
-        tooltip.querySelector('.share-selected-text-btn-twitter').href =
-            generateTwitterUrl(window.location.href, selected.text);
-
-        tooltip.querySelector('.share-selected-text-btn-buffer').href =
-            generateBufferUrl(window.location.href, selected.text);
+        Array.prototype.forEach.call(parameters.buttons, function (btn) {
+            tooltip.querySelector(`.share-selected-text-btn-${btn}`).href = generateSocialUrl(btn, selected.text);
+        });
 
         window.setTimeout(function () {
             showTooltip();
         }, 250);
     };
 
+    let generateAnchorTag = function (anchorType, customIconClass = null) {
+        let anchorTag = document.createElement('A');
+        let anchorIcon = document.createElement('i');
+
+        if (parameters.anchorsClass) {
+            anchorTag.classList.add(
+                'share-selected-text-btn',
+                `share-selected-text-btn-${anchorType}`,
+                `${parameters.anchorsClass}`
+            );
+        } else {
+            anchorTag.classList.add('share-selected-text-btn', `share-selected-text-btn-${anchorType}`);
+        }
+
+        if (customIconClass) {
+            anchorIcon.classList.add(`${customIconClass}`);
+        } else {
+            anchorIcon.classList.add(`icon-${anchorType}`, 'fa', `fa-${anchorType}`);
+        }
+
+        anchorIcon.style.pointerEvents = 'none';
+        anchorTag.addEventListener('click', function (e) {
+            e.preventDefault();
+            let windowFeatures = 'status=no,menubar=no,location=no,scrollbars=no,width=720,height=540';
+            let url = e.target.href;
+            window.open(url, 'Share this post', windowFeatures);
+        });
+
+        anchorTag.href = generateSocialUrl(anchorType, selected.text ? selected.text : '');
+        anchorTag.appendChild(anchorIcon);
+        return anchorTag;
+    };
+
     let generateTooltip = function () {
         let body = document.querySelector('body');
         let mainDiv = document.createElement('DIV');
         let btnContainer = document.createElement('DIV');
-        let twitterBtn = document.createElement('A');
-        let bufferBtn = document.createElement('A');
-        let twitterIcon = document.createElement('i');
-        let bufferIcon = document.createElement('i');
 
         mainDiv.classList.add('share-selected-text-main-container');
         btnContainer.classList.add('share-selected-text-inner');
 
-        twitterIcon.classList.add('fa', 'fa-twitter');
-        bufferIcon.classList.add('fa', 'fa-buffer');
-
-        twitterBtn.classList.add('share-selected-text-btn', 'share-selected-text-btn-twitter');
-        twitterBtn.href = '#';
-        bufferBtn.classList.add('share-selected-text-btn', 'share-selected-text-btn-buffer');
-        bufferBtn.href = '#';
-
-        twitterBtn.appendChild(twitterIcon);
-        bufferBtn.appendChild(bufferIcon);
+        if (parameters.tooltipClass) {
+            btnContainer.classList.add(parameters.tooltipClass);
+        }
 
         mainDiv.style.height = TOOLTIP_HEIGHT + 'px';
         mainDiv.style.top = 0;
         mainDiv.style.left = 0;
 
-        btnContainer.appendChild(twitterBtn);
-        btnContainer.appendChild(bufferBtn);
-        mainDiv.appendChild(btnContainer);
+        Array.prototype.forEach.call(parameters.buttons, function (btn) {
+            let aTag = generateAnchorTag(btn);
+                btnContainer.appendChild(aTag);
+        });
 
+        mainDiv.appendChild(btnContainer);
         body.appendChild(mainDiv);
 
         return mainDiv;
@@ -134,26 +175,28 @@
     };
 
     let shareTooltip = function () {
-        let selected = getSelectedText();
+        selected = getSelectedText();
 
         if (selected.text.length) {
             let oRange = selected.selection.getRangeAt(0);
             let oRect = oRange.getBoundingClientRect();
-            console.log(oRect);
-            console.log(selected);
-            updateTooltip(oRect, selected);
+            updateTooltip(oRect);
         } else {
             hideTooltip();
         }
     };
 
-    let shareSelectedText = function (element, args) {
+    exports.shareSelectedText = function (element, args) {
         let elt = document.querySelectorAll(element);
 
         parameters = extend({
-            tooltipClass: 'share-selected',
+            tooltipClass: '',
             sanitize: true,
-            twitterEnabled: true,
+            buttons: [
+                SOCIAL.twitter,
+                SOCIAL.buffer
+            ],
+            anchorsClass: '',
             twitterVia: 'PastaWS'
         }, args);
 
@@ -166,10 +209,8 @@
         });
     };
 
-    exports.shareSelectedText = shareSelectedText;
+    //exports.shareSelectedText = shareSelectedText;
 }(window));
-
-shareSelectedText('.blog-post-content');
 
 /*global jQuery, shareSelectedText*/
 if (window.jQuery) {
